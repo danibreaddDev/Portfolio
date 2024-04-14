9a71e9045038f37ccb7c1ac622a0cb7ae68301f4
Dani <danielpamblancomestre@gmail.com>
2024-04-14 20:43:03 +0200

    segunda version

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function Menu() {
+    const menu = document.querySelector(".navbar-nav");
+    const icon = document.querySelector(".navbar-toggler-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
